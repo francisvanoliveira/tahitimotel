88199cdab9f4521fcf7d752ad3e81f70ca740e18
francisvanoliveira <francisvan.oliveira@outlook.com>
2021-07-25 21:42:02 -0400

    :up: Atualização da sessão suíte

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
